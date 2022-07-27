56492c67d60f70e335ce395fe3758ef4dbc62cd5
ACER <azat010889@gmail.com>
2022-07-27 10:02:45 +0600

    init: ad Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 const App = () => {
   return (
-      <div>
-        
-      </div>
+     <>
+        <Header/>
+        <BrowserRouter>
+           <Routes>
+
+           </Routes>
+        </BrowserRouter>
+     </>
+
   );
 };
 
