e6f621890e50c5feb7d106cccf2f7f0d0c38c435
ACER <azat010889@gmail.com>
2022-07-28 09:46:12 +0600

    feat: Header finished

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Header from "./components/Header/Header";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 const App = () => {
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import logo from "../../media/logo/LOgo.svg"
+import phone from "../../media/logo/phone 1.svg"
+import vector from "../../media/logo/Vector.svg"
+import style from "./Header.module.css"
 
 const Header = () => {
     return (
-        <div>
-            <img src="" alt=""/>
-
+        <div className={style.header}>
+                <div className="">
+                    <img src={logo} alt=""/></div>
+                <ul className={style.list}>
+                    <li><a href="#">Главная</a></li>
+                    <li>
+                        <select name="" id="">
+                            <option value=""><a href="#">Меню</a></option>
+                            <option value=""><a href="#">Напитки</a></option>
+                            <option value=""><a href="#">Пицца</a></option>
+                        </select></li>
+                    <li><a href="#">Доставка</a></li>
+                    <li><a href="#">Контакты</a></li>
+                </ul>
+            <div className={style.phone}>
+                <img src={phone} alt=""/>
+                <span>+996500405988</span>
+            </div>
+            <div className={style.vector}>
+                <img src={vector} alt=""/>
+                <span>1</span>
+            </div>
         </div>
     );
 };
